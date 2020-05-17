a46fd3b7c57a9347171cc07d7f5675bfd4eb1856
Sheekaboom <aweiss7791@gmail.com>
2020-05-17 17:21:03 -0600

    theme continuity across all pages added

--- src/js/common_functs.js
+++ src/js/common_functs.js
@@ -1,3 +1,49 @@
+/*
+@brief Open/Close the navigation bar list
+*/
 function toggleNavbarList(){
     $('#common_navbar_links').slideToggle({direction:'left'})
 }
+
+/*
+@brief Set the website theme by setting 'data-theme' attribute of <html>
+@param[in] theme_value - value to set the theme. can be 'dark','light','default'
+*/
+function setTheme(theme_value){
+    // Get the current value
+    document.getElementsByTagName('html')[0].setAttribute('data-theme',theme_value); 
+    // store to viable location
+    if (typeof(Storage) !== "undefined") { // set the user values
+        localStorage.setItem('data-theme',theme_value);
+    } else { //otherwise use cookies
+        document.cookie = "data-theme="+theme_value+";path=/";
+    }
+}
+/*
+@brief Change the current theme from a <select> item
+@param[in] select_item - <select> html item to set theme from
+*/
+function themeSelectHandler(select_item){
+    var theme_str = select_item.value;
+    setTheme(theme_str);
+}
+
+/*
+@brief get a cookie value
+@cite https://www.w3schools.com/js/js_cookies.asp
+*/
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
--- src/js/load_common.js
+++ src/js/load_common.js
@@ -1,7 +1,45 @@
 $(function(){
-    //navigation bar and components
+    // initialize the user settings
+    initializeUserSettings();
+
+    // navigation bar and components
     $('#common_navbar').load('/common/nav.html'); //navigation bar
 
     // footer and components
     $('#common_footer').load('/common/footer.html'); //page footer
+
+    // update the UI to the current user settings
+    window.onload = initializeUIUserSettings();
 });
+
+/*
+@brief Initialize user settings on each page
+*/
+function initializeUserSettings(){
+    
+    // list of user settings and default values
+    var user_settings = ['data-theme'];
+    var default_user_setting_values = ['default'];
+
+    // set the values from storage
+    for(i=0;i<user_settings.length;i++){
+      if (typeof(Storage) !== "undefined") { // set the user values
+        var setting_value = localStorage.getItem(user_settings[i]) || default_user_setting_values[i];
+      } else {
+        var setting_value = getCookie(user_settings);
+      }
+      document.getElementsByTagName('html')[0].setAttribute(user_settings[i],setting_value);
+    }
+
+}
+
+/*
+@brief this will set all UI components on the screen to the correct values.forEach
+For example this will set the theme selector button to the current theme
+*/
+function initializeUIUserSettings(){
+    // Get the theme for setting out selector
+    var theme_value = document.getElementsByTagName('html')[0].getAttribute('data-theme');
+    document.getElementById('common_navbar_theme_selector').value = theme_value;
+}
+
